5ac3d0067b82dc7ce5c0765b96fe5807f3d0e4fd
mohamedeinteraface <interfacemohamede@gmail.com>
2025-12-03 08:40:16 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='static', template_folder='.')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# لتقديم الملفات الصوتية والصور (Flask يقدّم static بشكل تلقائي)
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
